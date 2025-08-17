ab47aae5e2928a1e82caee2d08e07eefc8389399
WilsonCSilva <wilso.carvalhais@gmail.com>
2025-08-17 11:58:47 -0300

    Add project files.

---
+++ FCG/Migrations/20250531202548_CorrecaoIdentityPedido.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FCG.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrecaoIdentityPedido : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
